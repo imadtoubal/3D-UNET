897968c5dcb69410eaf543db90830b9848c4d278
Imad Toubal <imad.toubal@gmail.com>
2020-02-12 00:06:00 -0600

    added 3d view functionality

--- vol3d.py
+++ vol3d.py
@@ -2,30 +2,21 @@
 # To add a new markdown cell, type '# %% [markdown]'
 import matplotlib
 matplotlib.use('tkagg')
-from matplotlib import pyplot as plt
-from matplotlib import style
-from matplotlib.widgets import Slider, Button
 import scipy.io as sio
 import numpy as np
+
+from tqdm import tqdm
 
 from skimage import feature
 
 import tkinter as tk
 from tkinter import filedialog
 
-root = tk.Tk()
-root.withdraw()
+# root = tk.Tk()
+# root.withdraw()
 
 # file_path = filedialog.askopenfilename()
 file_path = 'data/out/out0.mat'
-
-
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 3, 1)
-ax2 = fig.add_subplot(1, 3, 2)
-ax3 = fig.add_subplot(1, 3, 3)
-
-plt.subplots_adjust(left=0.1, bottom=0.35)
 
 def readmat(filename, var_name):
     img = sio.loadmat(filename)
@@ -47,31 +38,24 @@ def canny3d(seg):
 
 img = readmat(file_path, 'data')
 
-x, y, z, _ = np.shape(img)
-n = np.floor(z/2).astype('int')
-
-axSlider = plt.axes([0.1, 0.2, 0.8, 0.05])
-slider = Slider(axSlider, 'Slide', valmin=1, valmax=z, valstep=1, valinit=n)
-
 gt = img[:,:,:,2]
 seg = canny3d(img[:,:,:,1])
 im = img[:,:,:,0]
 
-ax1.set_title('3D MRI Image')
-ax2.set_title('Segmentation result')
-ax3.set_title('Ground Truth')
+import plotly.graph_objects as go
+x, y, z = seg.shape
 
-p1 = ax1.imshow(im[:,:,n])
-p2 = ax2.imshow(seg[:,:,n])
-p3 = ax3.imshow(gt[:,:,n])
- 
- 
-def frame_update(val):
-    n = np.floor(slider.val).astype('int') - 1
-    p1.set_data(im[:,:,n])
-    p2.set_data(seg[:,:,n])
-    p3.set_data(gt[:,:,n])
-
-slider.on_changed(frame_update)
-plt.show()
-
+X, Y, Z = np.mgrid[0:x:2, 0:y:2, 0:z:2]
+values = seg[X, Y, Z]
+print(X.shape, Y.shape, Z.shape)
+fig = go.Figure(data=go.Volume(
+    x=X.flatten(),
+    y=Y.flatten(),
+    z=Z.flatten(),
+    value=values.flatten(),
+    isomin=0.1,
+    isomax=0.8,
+    opacity=0.1, # needs to be small to see through all surfaces
+    surface_count=17, # needs to be a large number for good volume rendering
+    ))
+fig.show()
